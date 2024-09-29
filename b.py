59d9cf8cd1e2c4082d75ed7b8d57a324c6690a25
Misbah Noor <139441508+noorMisbah@users.noreply.github.com>
2024-09-29 19:53:19 +0500

    Update b.py

--- b.py
+++ b.py
@@ -75,8 +75,8 @@ def main():
         training_args = TrainingArguments(
             output_dir='./results',
             num_train_epochs=3,
-            per_device_train_batch_size=16,
-            per_device_eval_batch_size=64,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
             warmup_steps=500,
             weight_decay=0.01,
             logging_dir='./logs',
